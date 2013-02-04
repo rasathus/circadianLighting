4951bfc1825691696a7ca49c750c35dc4949507c
Chris Fane <chris@fane.cc>
2013-02-04 23:02:50 +0000

    A significantly more eloquent implementation, using a single queue, and taking advantage of the blocking get methods.  The implementation now uses APSchedular, for scheduling although im yet to implement persistence/automating loading of events

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import with_statement
-from socket import gethostname
 import logging
-from logging.handlers import RotatingFileHandler, SMTPHandler
 import time
-import datetime
-import sched
 import json
 import Queue
-from threading import Thread
 
+from logging.handlers import RotatingFileHandler
+from random import randint
+from threading import Thread
+from datetime import datetime, timedelta
 from flask import Flask, request, session, url_for, redirect, render_template, abort, g, flash, jsonify
+from apscheduler.scheduler import Scheduler
 
 from pigredients.ics import ws2801 as ws2801
 
@@ -20,20 +20,24 @@ LOGGING_PATH = "moodLight.log"
 
 led_chain = None
 auto_resume_offset = 30 # the number of idle minutes before resuming auto operation after a manual override.
+auto_resume_job = None
 
 # create our little application
 app = Flask(__name__)
 app.config.from_object(__name__)
-app.debug = True
+app.debug = True # !!! Set this to False for production use !!!
 
-time_format = "%H:%M:%S"
-# Event times should be in the form of %H:%M:%S
-# Event states should be in the form of [Red,Green,Blue]
-# Event names should be unique, as they are used for last run information
+persistent_store_path = '/home/pi/.lighting.db'
+
+config = {'apscheduler.jobstores.file.class': 'apscheduler.jobstores.shelve_store:ShelveJobStore',
+          'apscheduler.jobstores.file.path': persistent_store_path}
+
+
 auto_state_events = [{'event_name' :  'Night Phase', 'event_start_time' : '00:00:00', 'event_end_time' : '07:00:00' , 'event_state' : [0,0,0], 'transition_duration': 500},
                     {'event_name' :  'Sunrise Phase', 'event_start_time' : '07:30:00', 'event_end_time' : '08:00:00' , 'event_state' : [255,188,0], 'transition_duration': 5000},
                     {'event_name' :  'Alert Phase', 'event_start_time' : '08:00:00', 'event_end_time' : '21:00:00' , 'event_state' : [0,78,103], 'transition_duration': 500},
                     {'event_name' :  'Relaxation Phase', 'event_start_time' : '22:00:00', 'event_end_time' : '23:59:00' , 'event_state' : [255,0,0], 'transition_duration': 500},]
+
 
 # Stolen from http://stackoverflow.com/questions/4296249/how-do-i-convert-a-hex-triplet-to-an-rgb-tuple-and-back
 HEX = '0123456789abcdef'
@@ -47,35 +51,16 @@ def triplet(rgb):
     return format((rgb[0]<<16)|(rgb[1]<<8)|rgb[2], '06x')
 
 
-def unix_time(dt):
-    epoch = datetime.utcfromtimestamp(0)
-    delta = dt - epoch
-    return delta.total_seconds()
-
-def epoch_now():
-    epoch = datetime.utcfromtimestamp(0)
-    delta = datetime.now() - epoch
-    return delta.total_seconds()
-
-class Auto_Event:
-    def __init__(self, state, start_time):
-        self.start_time = datetime.datetime.time(datetime.datetime.strptime(start_time, time_format))
-        self.state = state
-
 class Chain_Communicator:
     
     def __init__(self):
         self.led_chain = ws2801.WS2801_Chain()
-        self.manual_queue = Queue.Queue()
-        self.auto_queue = Queue.Queue()
-        self.next_auto_event = None
+        self.auto_resume_job = None
+        self.queue = Queue.Queue()
         self.state = 'autonomous'
         self.last_ran = {}
         self.led_state = [0,0,0]
-        # populate our initial autoqueue state.
-        self.populate_auto_queue()
-
-        self.auto_resume = None
+        
         # use a running flag for our while loop
         self.run = True
         
@@ -84,123 +69,52 @@ class Chain_Communicator:
         self.loop_instance.start()
         app.logger.debug("Chain_Communicator init complete.")
 
-    def populate_auto_queue(self):            
-        # clear down the auto queue, don't know how long weve been in manual mode, queue may be stale.
-        with self.auto_queue.mutex:
-            self.auto_queue.queue.clear()
-            self.next_auto_event = None
-        # Calculating next state change.
-        current_time = datetime.datetime.time(datetime.datetime.now())
-        current_date = datetime.datetime.date(datetime.datetime.now())
-        app.logger.debug("Running populate_auto_queue @ current time : %s" % current_time)
-        for event in auto_state_events:
-            if event['event_name'] in self.last_ran.keys():
-                if self.last_ran[event['event_name']] != current_date:
-                    del self.last_ran[event['event_name']]
-                else:
-                    time.sleep(0.25)
-            
-            if event['event_name'] not in self.last_ran.keys():        
-                # check to see if we're in the middle of any scheduled events
-                start_time = datetime.datetime.time(datetime.datetime.strptime(event['event_start_time'],time_format))
-                end_time = datetime.datetime.time(datetime.datetime.strptime(event['event_end_time'],time_format))
-                # app.logger.debug("Checking to see whether current time : %s is between event start : %s and event end : %s" % (current_time, start_time, end_time))
-                if current_time > start_time and current_time < end_time:
-                    # got a match, this is our current event.
-                    app.logger.debug("Found a matching event, %s starts at %s and finishes %s" % (event['event_name'], event['event_start_time'], event['event_end_time']))
-                    # check we aren't already in that led state.
-                    if self.led_state != event['event_state']:
-                        self.auto_transition(event['event_state'], transition_duration=event['transition_duration'], start_time=event['event_start_time'])    
-                    self.last_ran[event['event_name']] = current_date
-                    break
-        else:
-            app.logger.debug("We don't appear to be within any programmed events.")  
-            time.sleep(0.25)
-
     def main_loop(self):
         app.logger.debug("main_loop - processing queue ...")
         while self.run :
-            # What about using two while loops one for while self.state == 'autonomous', 
-            # allowing much longer sleeps, and one for manual. will seb call to other thread pull out quick enough.
-            if self.state != 'autonomous':
-                # assume I need to do something for thread safety here ...
-                if not self.manual_queue.empty():
-                    manual_setting = self.manual_queue.get()
-                    self.led_chain.set_rgb(manual_setting)
-                    self.led_chain.write()
-                    self.led_state = manual_setting
-                    # !!! change this to minutes when done debugging !!!
-                    self.auto_resume = datetime.datetime.now() + datetime.timedelta(seconds=auto_resume_offset)
-                    self.manual_queue.task_done()
-                else:
-                    if datetime.datetime.now() > self.auto_resume:
-                        # Resuming autonomous operation, recalculating transitions to autonomous states.
-                        self.state = 'autonomous'
-                        app.logger.info("Resuming autonomous opperation")
-                        self.populate_auto_queue()
-                        # clear down our last run queue, as we may need to return to a previosuly executed state.
-                        self.last_ran = {}
-                    else:
-                        time.sleep(0.25)
-            else:
-                # run through our autonomous actions.
-                if self.next_auto_event is None:                
-                    if not self.auto_queue.empty():
-                        # pop the first off the top of the stack and check its start time
-                        self.next_auto_event = self.auto_queue.get()
-                    else:
-                        self.populate_auto_queue()
+            # Grab the next lighting event, block until there is one.
+            lighting_event = self.queue.get(block=True)
+            # set our chain state
+            self.led_chain.set_rgb(lighting_event)
+            # write out the previously set state.
+            self.led_chain.write()
+            # store our state for later comparisons.
+            self.led_state = lighting_event
 
-                if self.next_auto_event is not None:                
-                    current_time = datetime.datetime.time(datetime.datetime.now())
-                    if current_time >= self.next_auto_event.start_time:
-                        # write out our led state.
-                        self.led_chain.set_rgb(self.next_auto_event.state)
-                        self.led_chain.write()
-                        self.led_state = self.next_auto_event.state
-                        app.logger.debug("Auto-Event written out state : %s as event time : %s has passed" % (self.next_auto_event.state, self.next_auto_event.start_time)) 
-                        # reset our next event so we grab the next from the queue.
-                        self.next_auto_event = None
-                    else:
-                        app.logger.debug("Auto-Event time : %s is not past current time : %s" % (self.next_auto_event.start_time, current_time)) 
-            time.sleep(0.001)    
-
-
-    def auto_transition(self, state, transition_duration=150, start_time=datetime.datetime.now().strftime(time_format)):
+    def auto_transition(self, *args, **kwargs):
+        # accepts all events from scheduler, checks if in auto mode, if not throws them away.
+        if self.state is 'autonomous':
+            self.transition(*args, **kwargs)
+        
+    def transition(self, state, transition_duration=500, transition_mode='fade'):
         # States must be in the format of a list containing Red, Green and Blue element values in order.
         # example. White = [255,255,255] Red = [255,0,0] Blue = [0,0,255] etc.
         # a duration is represented in an incredibly imprescise unit, as ticks.
-        with self.manual_queue.mutex:
-            self.manual_queue.queue.clear()
-        app.logger.debug("Current state is : %s , destination state is : %s , transitioning in : %d ticks" % (self.led_state, state, transition_duration))
 
-        # Using a modified version of http://stackoverflow.com/questions/6455372/smooth-transition-between-two-states-dynamically-generated for smooth transitions between states.
-        for transition_count in range(transition_duration - 1):
-            event_state = []
-            for component in range(3):
-                event_state.append(self.led_state[component] + (state[component] - self.led_state[component]) * transition_count / transition_duration)
-            self.auto_queue.put(Auto_Event(event_state, start_time))
 
-        # last event is always fixed to the destination state to ensure we get there, regardless of any rounding errors. May need to rethink this mechanism, as I suspect small transitions will be prone to rounding errors resulting in a large final jump.
-        self.auto_queue.put(Auto_Event(state, start_time))
+        # !! Add in validation for transition modes !!
 
-    def manual_transition(self, state, transition_duration=150):
-        # States must be in the format of a list containing Red, Green and Blue element values in order.
-        # example. White = [255,255,255] Red = [255,0,0] Blue = [0,0,255] etc.
-        # a duration is represented in an incredibly imprescise unit, as ticks.
-        with self.manual_queue.mutex:
-            self.manual_queue.queue.clear()
-        app.logger.debug("Current state is : %s , destination state is : %s , transitioning in : %d ticks" % (self.led_state, state, transition_duration))
 
-        # Using a modified version of http://stackoverflow.com/questions/6455372/smooth-transition-between-two-states-dynamically-generated for smooth transitions between states.
-        for transition_count in range(transition_duration - 1):
-            event_state = []
-            for component in range(3):
-                event_state.append(self.led_state[component] + (state[component] - self.led_state[component]) * transition_count / transition_duration)
-            self.manual_queue.put(event_state)
+        with self.queue.mutex:
+            self.queue.queue.clear()
+        app.logger.debug("Current state is : %s , destination state is : %s , transitioning via %s in : %d ticks" % (self.led_state, state, transition_mode, transition_duration))
 
-        # last event is always fixed to the destination state to ensure we get there, regardless of any rounding errors. May need to rethink this mechanism, as I suspect small transitions will be prone to rounding errors resulting in a large final jump.
-        self.manual_queue.put(state)
+        if transition_mode is 'fade':        
+            # Using a modified version of http://stackoverflow.com/questions/6455372/smooth-transition-between-two-states-dynamically-generated for smooth transitions between states.
+            for transition_count in range(transition_duration - 1):
+                event_state = []
+                for component in range(3):
+                    event_state.append(self.led_state[component] + (state[component] - self.led_state[component]) * transition_count / transition_duration)
+                self.queue.put(event_state)
+
+            # last event is always fixed to the destination state to ensure we get there, regardless of any rounding errors. May need to rethink this mechanism, as I suspect small transitions will be prone to rounding errors resulting in a large final jump.
+            self.queue.put(state)
+
+    def resume_auto(self):
+        # returns system state to autonomous, to be triggered via the scheduler, or via a request hook from the web ui.
+        self.state = 'autonomous'
+        app.logger.debug("Resume auto called, system state is now : %s" % self.state)
+        app.logger.debug("Job list now contains : %s" % sched.print_jobs())
 
     def shutdown(self):
         app.logger.debug("shutdown - shutdown started ...")
@@ -224,15 +138,40 @@ def teardown_request(exception):
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/addjob')
+def add_job():
+    random_state = []
+    for i in range(3):
+        random_state.append(randint(0,255))
+    sched.add_date_job(led_chain.transition, datetime.now() + timedelta(seconds=10), kwargs={'state' : random_state})
+    app.logger.debug("Job list now contains : %s" % sched.print_jobs())
+    return jsonify({'sucess' : True}) 
     
 @app.route('/set/<hex_val>', methods=['GET', 'POST'])
 def send_command(hex_val):
     return_object = {'output' : None , 'error' : None, 'success' : False}
     rgb_val = rgb(hex_val)
     app.logger.debug("Given colour_val : %s, converted it to %s" % (hex_val, rgb_val))
-    led_chain.manual_transition([rgb_val['R'],rgb_val['G'],rgb_val['B']])
-    led_chain.state = 'manual'
+    led_chain.transition([rgb_val['R'],rgb_val['G'],rgb_val['B']])
     app.logger.info("Set chain to manual colour state : %s" % [rgb_val['R'],rgb_val['G'],rgb_val['B']])
+
+    # Set our schedulars auto resume time.
+    # !!! change this to minutes when finished debugging !!!
+    # large gracetime as we want to make sure it fires, regardless of how late it is.
+    for job in sched.get_jobs():
+        if job.name == 'autoresume':
+            app.logger.debug("Removing existing autoresume job, and adding a new one.")
+            sched.unschedule_job(led_chain.auto_resume_job)
+            led_chain.auto_resume_job = sched.add_date_job(led_chain.resume_auto, datetime.now() + timedelta(seconds=auto_resume_offset), name='autoresume', misfire_grace_time=240)
+            break
+    else:
+        app.logger.debug("No existing autoresume jobs, adding one.")
+        led_chain.auto_resume_job = sched.add_date_job(led_chain.resume_auto, datetime.now() + timedelta(seconds=auto_resume_offset), name='autoresume', misfire_grace_time=240)
+        
+    app.logger.debug("Job list now contains : %s" % sched.print_jobs())
+    led_chain.state = 'manual'
+
     return_object['success'] = True
     return jsonify(return_object)   
 
@@ -254,10 +193,13 @@ if __name__ == '__main__':
     
     led_chain = Chain_Communicator()
 
+    sched = Scheduler(config)
+    sched.start()
+    app.logger.debug("Startup job list contains : %s" % sched.get_jobs())
+
     try:
         app.run(host='0.0.0.0', port=8080, use_reloader=False)
     except KeyboardInterrupt:
         app.logger.warning("Caught keyboard interupt.  Shutting down ...")
         led_chain.shutdown()
-    except :
-        app.logger.exception("Generic error")
+        sched.shutdown(wait=False)
